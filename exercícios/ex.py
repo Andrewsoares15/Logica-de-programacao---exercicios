f17319cb08f1ad584bb102b1b029a08241f3f289
Andrew <70962775+Andrewsoares15@users.noreply.github.com>
2020-10-23 16:38:58 -0300

    Subindo arquivos

--- exercícios/ex.py
+++ exercícios/ex.py
@@ -1,5 +1,5 @@
 #Escreva um programa que calcule o tempo de uma viagem de carro.
-#Pergunte a distância a percorrer e a velocidade média esperada para viagem#
+#Pergunte a distância a percorrer e a velocidade média esperada para viagemm#
 dist = float(input("Qual distância a ser percorrida"))
 vm = float(input("Digite a velocidade média"))
 tempo= dist / vm 
