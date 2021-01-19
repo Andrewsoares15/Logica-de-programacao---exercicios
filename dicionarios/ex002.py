72fdfc16a799d52822afbd424997f9919280562d
Andrew Soares <andrewsoares347@gmail.com>
2021-01-18 21:09:56 -0300

    revisando e praticando

---
+++ dicionarios/ex002.py
@@ -0,0 +1,8 @@
+filme={'titulo': 'stars wars',
+'ano': 1977,
+'diretor': 'helbert richards'
+}
+print("O diretor do filme é: {}, e o ano de: {}".format(filme['diretor'], filme['ano']))
+print(filme.keys()) # serve para pegar as chaves
+print(filme.values())# serve para pegar os valores
+print(filme.items()) #serve para pegar tudo
